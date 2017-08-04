613be9d4bbf9b2490cf8ea6c233d0c146f7d05b5
Niko Klaneček <klanecek.niko@gmail.com>
2017-08-04 14:04:11 +0200

    removed parentheses

--- wednesday.py
+++ wednesday.py
@@ -1,6 +1,6 @@
 import datetime
 
-if (datetime.datetime.today().weekday()) == 2:
+if datetime.datetime.today().weekday() == 2:
     print('It is Wednesday my dude')
 else:
     print('It is not Wednesday my dude')
