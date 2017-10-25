f98a8f2cd396bb1d856294882e4fb17cc835f5d2
Niko Klaneček <klanecek.niko@gmail.com>
2017-10-25 21:12:43 +0200

    removed else statement
    the message now only prints on wednesday

--- wednesday.py
+++ wednesday.py
@@ -1,10 +1,5 @@
 import datetime
 
+
 if datetime.datetime.today().weekday() == 2:
     print('It is Wednesday my dude')
-# remove everything after this point
-# if you want the statement to only print on Wednesday
-else:
-    print('It is not Wednesday my dude')
-
-# add to bashrc to make it print out
